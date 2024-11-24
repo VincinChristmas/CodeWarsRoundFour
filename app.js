2008f4de16a8c73df43d946ccfbd2475e048d721
VincinChristmas <VincinChristmas@hotmail.com>
2024-11-23 20:38:45 -0600

    refactored an old kata

--- app.js
+++ app.js
@@ -12,6 +12,49 @@
 /*********THE ABOVE IS A TEMPLATE*************** */
 
 
+//NUMBER 7
+
+/*
+Coding in function rndCode. Your task is to generate a random verification code. In accordance with the following rules:
+
+the code length should be 8;
+
+The 1st and 2nd characters should be two uppercase letters. The range is "ABCDEFGHIJKLM".
+
+The 3rd-6th characters should be four numbers.
+
+the 7th and 8th characters should be two symbols. The range is "~!@#$%^&*".
+
+If Your code runs 100 times, It should generate 100 non duplicate verification codes.
+
+Some valid verification code examples:
+
+AB1234#$ MG6145$@ KJ2249@&
+CD5678%^ IG7593~% FH8638@&
+EF9012!@ GB7047%$ KD7604^%
+*/                       
+
+//MY SOLUTION
+function rndCode() {
+  let charBox = "ABCDEFGHIJKLM";
+  let specialCharBox = "~!@#$%^&*";
+  let charHouse = [];
+  
+  for (let i = 0; i < 2; i++) {
+    charHouse.push(charBox[Math.floor(charBox.length * Math.random())]);
+  }
+  for (let i = 0; i < 4; i++) {
+    charHouse.push(Math.floor(10 * Math.random()));
+  }
+  for (let i = 0; i < 2; i++) {
+    charHouse.push(specialCharBox[Math.floor(specialCharBox.length * Math.random())]);
+  }
+
+  return charHouse.join('');
+}
+
+
+
 //NUMBER 6
 
 /*
