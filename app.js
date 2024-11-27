c69a8a69983c4151a6b4b6f474ab0ecc47cc0719
VincinChristmas <VincinChristmas@hotmail.com>
2024-11-27 15:38:44 -0600

    added a new solution to an old  kata today

--- app.js
+++ app.js
@@ -10,6 +10,36 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+//NUMBER 9
+
+/*
+Complete function padIt, which accepts 2 parameters:
+
+str: a string representing the string to pad. We will pad it with "*" symbols on its left side and on its right side.
+n: a number indicating how many times to pad the string.
+Behaviour
+You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, 
+alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+Finally, return the padded string.
+*/                       
+
+//MY SOLUTION
+function padIt(str,n){
+  let sum = 0
+  while (sum < n) {
+    sum = sum + 1
+  
+  if (sum % 2 !== 0) {
+    str = '*' + str
+  }
+  else if (sum % 2 == 0) {
+    str = str + '*'
+  }
+    }
+  return str
+}
+
 //NUMBER 8
 
 /*
