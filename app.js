dfc5bf08feedcc105868bcf2f5af710e5db7aeb7
VincinChristmas <VincinChristmas@hotmail.com>
2024-11-27 15:52:17 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -10,6 +10,32 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+
+//NUMBER 10
+
+/*
+Implement the function generateRange which takes three arguments (start, stop, step) and returns the range of integers from start to stop (inclusive) 
+in increments of step.
+
+Examples
+(1, 10, 1) -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+(-10, 1, 1) -> [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1]
+(1, 15, 20) -> [1]
+*/                       
+
+//MY SOLUTION
+function generateRange(min, max, step){
+  let collection = []
+  while (min <= max) {
+    collection.push(min)
+    min = min + step
+    
+  }
+    return collection
+  
+  }
+
 //NUMBER 9
 
 /*
