95c5cdc700ee442ee205621a08a1550afbaf21ec
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-01 19:24:38 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -11,6 +11,41 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 14
+
+/*
+Switch/Case - Bug Fixing #6
+Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+*/                       
+
+//MY SOLUTION
+
+
+function evalObject(value){
+  var result = 0;
+  switch(value.operation){
+    case'+': 
+      result = value.a + value.b;
+      break;
+    case'-': 
+      result = value.a - value.b;
+      break;
+    case'/': 
+      result = value.a / value.b;
+      break;
+    case'*': 
+      result = value.a * value.b;
+      break;
+    case'%': 
+      result = value.a % value.b;
+      break;
+    case'^': 
+      result = Math.pow(value.a, value.b);
+      break;
+  }
+  return result;
+}
+
 //NUMBER 13
 
 /*
