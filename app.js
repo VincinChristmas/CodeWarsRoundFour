891a9528c328c4489bc0120e2b566decffb2a47b
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-05 22:14:09 -0600

    added a javascript solution to a problem that I had recently solved using php

--- app.js
+++ app.js
@@ -10,6 +10,33 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+
+//NUMBER 16
+
+/*
+In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+
+*/                       
+
+//MY SOLUTION
+function periodIsLate(last, today, cycleLength) {
+  let diffTime = today - last
+  let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays > cycleLength;
+  
+}
+
 //NUMBER 15
 
 /*
