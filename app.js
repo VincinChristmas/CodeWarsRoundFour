cbe0ed359ea863b34feeae50dfabb8c800955372
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-08 15:45:38 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -11,6 +11,30 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 17
+
+/*
+Two players - "black" and "white" are playing a game. The game consists of several rounds. If a player wins in a round, he is to move again during the next round. 
+If a player loses a round, it's the other player who moves on the next round. Given whose turn it was on the previous round and whether he won, determine whose turn 
+it is on the next round.
+*/                       
+
+//MY SOLUTION
+function whoseMove(lastPlayer, win) {
+  if (lastPlayer === "black" && win === false) {
+    return "white"
+  }
+  else if (lastPlayer === "white" && win === true) {
+    return "white"
+  }
+   else if (lastPlayer === "white" && win === false) {
+    return "black"
+  }
+  else if (lastPlayer === "black" && win === true) {
+    return "black"
+  }
+}
+
 
 //NUMBER 16
 
