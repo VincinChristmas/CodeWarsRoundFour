a4de4b06477a9e57eec6aa256d165c4e5cc3143c
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-21 22:19:24 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -11,7 +11,36 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
-//NUMBER 
+
+//NUMBER 24
+
+/*
+You are creating an "Escape the room" game. The first step is to create a hash table ( dict in Python) called rooms
+ that contains all of the rooms of the game. There should be at 
+least 3 rooms inside it, each room being a hash table with at least 3 properties (e.g. name, description, completed).
+*/                       
+
+//MY SOLUTION
+// Add rooms here
+var rooms = {
+  name: {
+    room: "room1",
+    room2: "room two",
+    room3: "room three"
+  },
+  description: {
+    thisRoom: "dark room",
+    otherRoom: "light room",
+    backRoon: "rear Room"
+                },
+    completed: {
+    BobsRoom: "No",
+    LisasRooms: "Yes",
+    FredsRoom: "Unknown"
+    }
+  }
+
+//NUMBER 23
 
 /*
 Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block
