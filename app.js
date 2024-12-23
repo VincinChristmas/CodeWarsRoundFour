b5d863fc664379bde4e300ee93ba11ad868ea914
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-22 18:42:55 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -12,6 +12,44 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 25
+
+/*
+The number n is Evil if it has an even number of 1's in its binary representation.
+The first few Evil numbers: 3, 5, 6, 9, 10, 12, 15, 17, 18, 20
+
+The number n is Odious if it has an odd number of 1's in its binary representation.
+The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
+
+You have to write a function that determine if a number is Evil of Odious, function should return "It's Evil!" 
+in case of evil number and "It's Odious!" in case of odious number.
+
+good luck :)
+*/                       
+
+//MY SOLUTION
+
+function evil(n) {
+  let sum = 0
+  let newN = n.toString(2).split('')
+  for (let i = 0; i <= newN.length; i++) {
+    if (newN[i] === '1') {
+      sum = sum + 1
+    }
+    else {
+      sum = sum + 0
+    }
+    
+  }
+if (sum % 2 !== 0) {
+      return "It's Odious!"
+    }
+else if (sum % 2 === 0) {
+      return "It's Evil!"
+    }
+  
+}
+
 //NUMBER 24
 
 /*
