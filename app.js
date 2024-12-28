5fc909be8f4c782d33ce47bece264870401dcf3c
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-28 15:17:27 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -12,6 +12,36 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 28
+
+/*
+Check your arrows
+You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an 
+optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+You need to verify that you have some good ones left, in order to prepare for battle:
+
+anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
+If an arrow in the quiver does not have a damaged status, it means it's new.
+
+The expected result is a boolean, indicating whether you have any good arrows left.
+*/                       
+
+//MY SOLUTION
+
+function anyArrows(arrows){
+  if (arrows.length === 0) {
+    return false
+  }
+  for (let i = 0; i < arrows.length; i++) {
+    const arrow = arrows[i]
+    const damagedValue = arrow.damaged
+    if (typeof damagedValue === 'undefined'|| !damagedValue) {
+      return true
+    }
+  }
+  return false
+}
+
 //NUMBER 27
 
 /*
