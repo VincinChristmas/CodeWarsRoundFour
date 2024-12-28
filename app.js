2d05291b29dcb1fd629524593e65f158681f557b
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-27 22:28:53 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -12,6 +12,29 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 27
+
+/*
+You'll be given a string, and have to return the sum of all characters as an int. 
+The function should be able to handle all printable ASCII characters.
+
+Examples:
+
+uniTotal("a") == 97
+uniTotal("aaa") == 291
+*/                       
+
+//MY SOLUTION
+function uniTotal (string) {
+  let asciiCodes = 0
+ 
+    for (let i = 0; i < string.length; i++) {
+      asciiCodes += string.charCodeAt(i)
+    }
+  
+  return asciiCodes
+}
+
 //NUMBER 26
 
 /*
