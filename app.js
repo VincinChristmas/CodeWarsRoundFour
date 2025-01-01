379afe77fd83c0a6001fbe391a4d9e530243f1fa
VincinChristmas <VincinChristmas@hotmail.com>
2024-12-31 21:52:28 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -7,10 +7,35 @@
 //MY SOLUTION
 
 
-
-
-
 /*********THE ABOVE IS A TEMPLATE*************** */
+
+//NUMBER 32
+
+/*
+Given an array of 4 integers
+[a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+
+For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+
+   a:x1
+   b:y1
+   c:x2
+   d:y2
+Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). 
+*/                       
+
+//MY SOLUTION
+function slope(points)
+{
+let topPortion = points[3] - points[1]
+let bottomPortion = points[2] - points[0]
+if (bottomPortion === 0) {
+  return "undefined"
+}
+  else {
+    return (topPortion / bottomPortion).toString()
+  }
+}
 
 //NUMBER 31
 
