1c91952b0469e6b98a2ad2a53d87df1a486485de
VincinChristmas <VincinChristmas@hotmail.com>
2025-01-01 20:13:23 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -8,6 +8,22 @@
 
 
 /*********THE ABOVE IS A TEMPLATE*************** */
+//NUMBER 39
+
+/*
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, 
+unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output)
+
+"Yes" --> "Yes?" 
+"No?" --> "No?"
+*/                       
+
+//MY SOLUTION
+function ensureQuestion(s) {
+  return (s.includes('?')) ? s : s + '?'
+}
 
 //NUMBER 38
 
