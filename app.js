7e408905e2e8c6a85b69f51b8858a1c2ebb075ed
VincinChristmas <VincinChristmas@hotmail.com>
2025-01-04 17:34:54 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -9,6 +9,33 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 45
+
+/*
+For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. 
+If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to 
+return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+
+Please leave feedback for this kata. Cheers!
+*/                       
+
+//MY SOLUTION
+function cookie(x){
+  if (typeof x === "string") {
+    return "Who ate the last cookie? It was Zach!"
+  }
+   else if (x === Number(x)) {
+     return "Who ate the last cookie? It was Monica!"
+   }
+   else {
+     return "Who ate the last cookie? It was the dog!"
+   }
+ }
+
 //NUMBER 44
 
 /*
