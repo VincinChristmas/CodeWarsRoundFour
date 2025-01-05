87e8f7291e4730e8f663dc9f57c24333b81513af
VincinChristmas <VincinChristmas@hotmail.com>
2025-01-05 17:22:49 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -9,6 +9,22 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 62
+
+/*
+In JavaScript, there is a special case where strict comparison of the same variable returns false! 
+Try to find out what must be done to get such result!
+
+var x = something;
+x === x // returns false!
+Write a function which will return value for which strict comparison will give false!
+*/                       
+
+//MY SOLUTION
+function findStrangeValue() {
+  return NaN
+}
+
 //NUMBER 61
 
 /*
