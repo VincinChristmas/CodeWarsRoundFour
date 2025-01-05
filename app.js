aaac3b68fc2b838b88b32cd8b7d8bca30b63efb8
VincinChristmas <VincinChristmas@hotmail.com>
2025-01-05 17:38:25 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -9,6 +9,29 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 64
+
+/*
+Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value?
+ Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+*/                       
+
+//MY SOLUTION
+
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  return getMax1()
+}
+
 //NUMBER 63
 
 /*
