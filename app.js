08b5b1a636e04d185aaa7d7f00cd3e280375ed92
VincinChristmas <VincinChristmas@hotmail.com>
2025-01-22 19:24:10 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -9,6 +9,66 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 67 -- in progress
+
+/*
+given an array of integers, which represent the stones, and your task is 
+to assemble the highest-value pyramid from them. The pyramid is made out of exactly three layers, containing:
+
+Top layer: 1 integer
+Middle layer: 2 identical integers
+Bottom layer: 3 identical integers
+Additionally, no integer can appear in two or more layers. That is, 
+each layer is made from stones of the same value, and stones of the same value can be used at most in one layer.
+
+Graphically, the structure of the pyramid looks like this, where 
+x
+≠
+y
+≠
+z
+x
+
+=y
+
+=z
+
+    [z]
+  [y] [y]
+ [x][x][x]
+Input
+The input is an array that may contain positive and negative integers, 
+as well as zeros. The integers are in no specific order and can be repeated. The array may also be empty.
+
+Output
+The output is a single integer – the sum of all integers that make up the pyramid.
+ For example, given input [1,1,1,2,2,2,3,3,3], the highest-value pyramid is:
+
+    [1]
+  [2] [2]
+ [3][3][3]
+And the sum is thus: 
+3
+⋅
+3
++
+2
+⋅
+2
++
+1
+=
+14
+3⋅3+2⋅2+1=14.
+
+If it's not possible to build a 
+pyramid from the given array (e.g. [-1,-1,0,0,1,1] or an empty one []), return None, null, 
+or other language-specific alternative.
+*/
+
+//MY SOLUTION
+
+
 //NUMBER 66 failed kata
 
 /*
