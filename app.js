92940803b75135a261187e87a1c6eeaae9f7312c
VincinChristmas <VincinChristmas@hotmail.com>
2025-03-02 17:28:59 -0600

    working on a new kata

--- app.js
+++ app.js
@@ -9,6 +9,37 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 70
+
+/*
+You're given a two-dimensional array of integers matrix. Your task is to determine 
+the smallest non-negative integer that is not present in this array.
+
+Input/Output
+[input] 2D integer array matrix
+A non-empty rectangular matrix.
+
+1 ≤ matrix.length ≤ 10
+
+1 ≤ matrix[0].length ≤ 10
+
+[output] an integer
+The smallest non-negative integer that is not present in matrix.
+
+Example
+For
+
+  matrix = [  [0, 2],  [5, 1]] the result should be 3
+
+0,1,2,(3)...5
+*/                       
+
+//MY SOLUTION
+function smallestInteger(matrix) {
+  let firstMap = matrix.flat().sort((a,b)=>a-b);
+  return firstMap;
+}
+
 //NUMBER 69
 
 /*
