23de4daf36ac8f4b0fd401fb6d778658979c024b
VincinChristmas <VincinChristmas@hotmail.com>
2025-03-01 23:33:53 -0600

    added a new kata today

--- app.js
+++ app.js
@@ -9,6 +9,37 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 69
+
+/*
+Step through my green glass door.
+
+You can take the moon, but not the sun.
+
+You can take your slippers, but not your sandals.
+
+You can go through yelling, but not shouting.
+
+You can't run through fast, but you can run with speed.
+
+You can take a sheet, but not your blanket.
+
+You can wear your glasses, but not your contacts.
+
+Have you figured it out? Good! Then write a program that can figure it out as well.
+*/                       
+
+//MY SOLUTION
+function stepThroughWith(s) {
+  s = s.toLowerCase();
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
 //NUMBER 68 failed kata
 
 /*
