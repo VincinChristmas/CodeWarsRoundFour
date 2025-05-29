4b38f7ded1e02e02a47e373a4da3c1004302cd4b
VincinChristmas <VincinChristmas@hotmail.com>
2025-05-29 17:45:41 -0500

    added a new kata today

--- app.js
+++ app.js
@@ -9,6 +9,45 @@
 
 /*********THE ABOVE IS A TEMPLATE*************** */
 
+//NUMBER 90 
+
+/*
+https://www.codewars.com/kata/55b75fcf67e558d3750000a3
+*/                       
+
+//MY SOLUTION
+class Block{
+
+  constructor([width, length, height]){
+    this.width = width
+    this.length = length
+    this.height = height
+  }
+  getWidth() {
+    return this.width
+  }
+  
+  getLength() {
+    return this.length
+  }
+  
+  getHeight() {
+    return this.height
+  }
+  
+  getVolume() {
+    return this.width * this.length * this.height;
+  }
+  
+  
+  getSurfaceArea() {
+    return 2 * (this.width * this.length + 
+                this.width * this.height + 
+                this.length * this.height);
+  }
+  
+}
+
 
 //NUMBER 89
 
